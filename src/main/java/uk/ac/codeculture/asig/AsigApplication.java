4ae1f2877a339fe0d1d0974ecb4d0ee4f2953ada
shez1326 <jackshez1326@hotmail.com>
2018-02-28 12:34:39 +0000

    Add files via upload

---
+++ src/main/java/uk/ac/codeculture/asig/AsigApplication.java
@@ -0,0 +1,12 @@
+package uk.ac.codeculture.asig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsigApplication.class, args);
+	}
+}
